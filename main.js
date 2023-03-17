88dd0650c234c6d49f226e0451928462181dbbf6
Sohib <sohib19950133@gmail.com>
2023-03-17 09:42:29 +0500

    lesson15

--- main.js
+++ main.js
@@ -8,12 +8,32 @@ let personalMovieDB = {
 
 };
 
-let a = prompt("Odin i poslednix prosmotrennix filmov?"),
-    b = prompt("Na skolko otsenite ego?"),
-    c = prompt("Odin i poslednix prosmotrennix filmov?"),
-    d = prompt("Odin i poslednix prosmotrennix filmov?");
 
-    personalMovieDB.movies [a] = b;
-    personalMovieDB.movies [c] = d;
+
+    let a ,
+        b;
+
+    for(let i = 0; i < 2; i++){
+       a = prompt("Odin i poslednix prosmotrennix filmov?", '');
+      b = prompt("Na skolko otsenite ego?", "");
+
+      if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies [a] = b;
+      } else {
+        console.log('error');
+        i--;
+      }
+     
+    }
+
+    if(personalMovieDB.count < 10){
+      console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count <= 30 && personalMovieDB.count >= 10){
+      console.log("Вы классический зритель");
+    } else if (personalMovieDB.count > 30){
+      console.log("Вы киноман");
+    }else {
+      console.log("Произошла ошибка");
+    }
 
  console.log(personalMovieDB);   
